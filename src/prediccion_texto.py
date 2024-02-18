dd14bfa13a0a79fdde7468d967dcb93a39c7cb70
PatoBlando <agustintblando@gmail.com>
2024-02-18 20:49:51 -0300

    Primer version de la prediccion de textos: * Implementado el uso de bigramas para la predicción de texto basado en la palabra siguiente o la anterior

--- src/prediccion_texto.py
+++ src/prediccion_texto.py
@@ -1,10 +1,7 @@
 import sys
 import random
 import json
-
-
-def predecir():
-    pass
+from collections import defaultdict
 
 def get_entrada(nombre):
     try:
@@ -23,38 +20,38 @@ def get_entrada(nombre):
         print("ERROR: No se ingresó el nombre del archivo", file=sys.stderr)
         sys.exit(1)
 
-def contar_ocurrencias(oracion, texto):
-    ocurrencias = 0
-    for i in range(len(texto)):
-        if texto[i] == oracion:
-            ocurrencias += 1
-    return ocurrencias
-
-def subcadenas(oracion, texto):
-    subcadenas = []
-    for i in range(len(oracion)):
-        subcadenas.append(oracion[i:])
-    return subcadenas
-
-def modelo_oracion(oracion, texto):
-    modelo = {}
-    for subcadena in subcadenas(oracion, texto):
-        ocurrencias = 0
-        modelo[tuple(subcadena)] = 0 
-    return modelo
-
-def generar_modelos(frases, texto):
-    modelos = {}
-    for frase in frases:
-        modelo = modelo_oracion(frase, texto)
-        print(modelo)
-        if modelo:
-            modelos[tuple(frase)] = modelo
-    return modelos
-
+def generar_ngrama(words, n):
+    return [tuple(words[i:i+n]) for i in range(len(words)-n+1)]
 
 def predecir(oracion, modelo):
-    pass
+    idx = oracion.index("_")
+    pred_prev_value = 0
+    pred_sig_value = 0
+    if idx > 0:
+        prev_word = oracion[idx-1]
+        if modelo[prev_word]:
+            pred_sig_key, pred_sig_value = max(modelo[prev_word]["sig"].items(), key=lambda item: item[1])
+    if idx < len(oracion) - 1:
+        sig_word = oracion[idx+1]
+        if modelo[sig_word]:
+            pred_prev_key, pred_prev_value = max(modelo[sig_word]["prev"].items(), key=lambda item: item[1])
+    if pred_sig_value == 0 and pred_prev_value == 0:
+        return "unknown"
+    return pred_sig_key if pred_sig_value > pred_prev_value else pred_prev_key
+        
+
+def entrenar_modelo(texto):
+    modelo = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
+    for oracion in texto:
+        for w1, w2 in generar_ngrama(oracion, 2):
+            modelo[w1]["sig"][w2] += 1
+            modelo[w2]["prev"][w1] += 1
+    return modelo
+
+def predecir_oraciones(frases, modelo):
+    for oracion in frases:
+        # predecir(oracion, modelo)
+        print("La predicción para la oración", oracion, "es", predecir(oracion, modelo))
 
 #Lee las oraciones de Frases/{{arg}}.txt y las devuelve en una lista de listas (frases)
 def leer_frases(nombre):
@@ -79,10 +76,11 @@ def main():
         sys.exit(1)
     entrada = get_entrada(nombre)
     frases = leer_frases(nombre)
-    modelos = generar_modelos(frases, entrada)
+    modelos = entrenar_modelo(entrada)
+    predecir_oraciones(frases, modelos)
     with open('debugg.json', 'w') as f:
         modelos_str = {str(k): {str(sub_k): sub_v for sub_k, sub_v in v.items()} for k, v in modelos.items()}
-        json.dump({"entrada": entrada, "frases": frases, "modelos": modelos_str}, f, indent=4)
+        json.dump({"modelos": modelos_str, "frases": frases}, f, indent=4)
     
 
 if __name__ == "__main__":
